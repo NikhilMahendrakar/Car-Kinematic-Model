dc9a86ac5cef5179b3d8f9c6db81f0d9faefac2c
NikhilMahendrakar <nikhilmahendrakar00@gmail.com>
2024-03-17 17:30:41 -0700

    Implemented Hybrid A*

--- pathplanning.py
+++ pathplanning.py
@@ -2,9 +2,6 @@ import numpy as np
 import math
 import scipy.interpolate as scipy_interpolate
 
-
-import math
-import numpy as np
 import os
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as font_manager
@@ -21,14 +18,16 @@ def make_square(x, y, width):
 
 
 
+############################################## Functions ######################################################
+
 def interpolate_b_spline_path(x, y, n_path_points, degree=3):    
     ipl_t = np.linspace(0.0, len(x) - 1, len(x))
     spl_i_x = scipy_interpolate.make_interp_spline(ipl_t, x, k=degree)
     spl_i_y = scipy_interpolate.make_interp_spline(ipl_t, y, k=degree)
     travel = np.linspace(0.0, len(x) - 1, n_path_points)
     return spl_i_x(travel), spl_i_y(travel)
- 
-def interpolate_path(path, sample_rate):      
+
+def interpolate_path(path, sample_rate):       
     choices = np.arange(0,len(path),sample_rate)
     if len(path)-1 not in choices:
             choices =  np.append(choices , len(path)-1)
@@ -40,17 +39,17 @@ def interpolate_path(path, sample_rate):
     # new_path[new_path<0] = 0
     return new_path
 
+################################################ Path Planner ################################################
 
-class AStarPlanner:     # Changes Required 
-
+class AStarPlanner:
     def __init__(self, ox, oy, resolution, rr):
         """
-        Initialize grid map for a star planning
+        Initialize grid map for hybrid A* planning
 
-        ox: x position list of Obstacles [m]
-        oy: y position list of Obstacles [m]
+        ox: x position list of obstacles [m]
+        oy: y position list of obstacles [m]
         resolution: grid resolution [m]
-        rr: robot radius[m]
+        rr: robot radius [m]
         """
 
         self.resolution = resolution
@@ -63,35 +62,42 @@ class AStarPlanner:     # Changes Required
         self.calc_obstacle_map(ox, oy)
 
     class Node:
-        def __init__(self, x, y, cost, parent_index):
+        def __init__(self, x, y, yaw, f, g, parent_index):
             self.x = x  # index of grid
             self.y = y  # index of grid
-            self.cost = cost
+            self.yaw = yaw  # angle [rad]
+            self.f = f
+            self.g = g
             self.parent_index = parent_index
 
         def __str__(self):
-            return str(self.x) + "," + str(self.y) + "," + str(
-                self.cost) + "," + str(self.parent_index)
+            return str(self.x) + "," + str(self.y) + "," + str(self.yaw) + "," + str(self.f) + "," + str(
+                self.g) + "," + str(self.parent_index)
 
-    def planning(self, sx, sy, gx, gy):
+    def planning(self, sx, sy, syaw, gx, gy, gyaw):
         """
-        A star path search
+        Hybrid A* path search
 
         input:
-            s_x: start x position [m]
-            s_y: start y position [m]
+            sx: start x position [m]
+            sy: start y position [m]
+            syaw: start yaw angle [rad]
             gx: goal x position [m]
             gy: goal y position [m]
+            gyaw: goal yaw angle [rad]
 
         output:
             rx: x position list of the final path
             ry: y position list of the final path
+            ryaw: yaw angle list of the final path
         """
 
         start_node = self.Node(self.calc_xy_index(sx, self.min_x),
-                               self.calc_xy_index(sy, self.min_y), 0.0, -1)
+                               self.calc_xy_index(sy, self.min_y),
+                               self.calc_yaw_index(syaw), 0.0, 0.0, -1)
         goal_node = self.Node(self.calc_xy_index(gx, self.min_x),
-                              self.calc_xy_index(gy, self.min_y), 0.0, -1)
+                              self.calc_xy_index(gy, self.min_y),
+                              self.calc_yaw_index(gyaw), 0.0, 0.0, -1)
 
         open_set, closed_set = dict(), dict()
         open_set[self.calc_grid_index(start_node)] = start_node
@@ -103,28 +109,28 @@ class AStarPlanner:     # Changes Required
 
             c_id = min(
                 open_set,
-                key=lambda o: open_set[o].cost + self.calc_heuristic(goal_node,
-                                                                     open_set[
-                                                                         o]))
+                key=lambda o: open_set[o].f)
             current = open_set[c_id]
 
-            if current.x == goal_node.x and current.y == goal_node.y:
-                print("Find goal 2")
+            if current.x == goal_node.x and current.y == goal_node.y and current.yaw == goal_node.yaw:
+                print("Find goal")
                 goal_node.parent_index = current.parent_index
-                goal_node.cost = current.cost
+                goal_node.f = current.f
+                goal_node.g = current.g
                 break
 
-            # Remove the item from the open set
+            # Removing the item from the open set
             del open_set[c_id]
 
-            # Add it to the closed set
+            # Adding it to the closed set
             closed_set[c_id] = current
 
             # expand_grid search grid based on motion model
             for i, _ in enumerate(self.motion):
                 node = self.Node(current.x + self.motion[i][0],
                                  current.y + self.motion[i][1],
-                                 current.cost + self.motion[i][2], c_id)
+                                 (current.yaw + self.motion[i][2]) % self.calc_yaw_index(math.pi * 2),
+                                 0.0, 0.0, c_id)
                 n_id = self.calc_grid_index(node)
 
                 # If the node is not safe, do nothing
@@ -137,32 +143,37 @@ class AStarPlanner:     # Changes Required
                 if n_id not in open_set:
                     open_set[n_id] = node  # discovered a new node
                 else:
-                    if open_set[n_id].cost > node.cost:
+                    if open_set[n_id].g > node.g:
                         # This path is the best until now. record it
                         open_set[n_id] = node
 
-        rx, ry = self.calc_final_path(goal_node, closed_set)
+        rx, ry, ryaw = self.calc_final_path(goal_node, closed_set)
 
-        return rx, ry
+        return rx, ry, ryaw
 
     def calc_final_path(self, goal_node, closed_set):
         # generate final course
-        rx, ry = [self.calc_grid_position(goal_node.x, self.min_x)], [
-            self.calc_grid_position(goal_node.y, self.min_y)]
+        rx, ry, ryaw = [self.calc_grid_position(goal_node.x, self.min_x)], [
+            self.calc_grid_position(goal_node.y, self.min_y)], [
+                            self.calc_yaw_angle(goal_node.yaw)]
         parent_index = goal_node.parent_index
         while parent_index != -1:
             n = closed_set[parent_index]
             rx.append(self.calc_grid_position(n.x, self.min_x))
             ry.append(self.calc_grid_position(n.y, self.min_y))
+            ryaw.append(self.calc_yaw_angle(n.yaw))
             parent_index = n.parent_index
 
-        return rx, ry
+        return rx, ry, ryaw
+
+    def calc_yaw_angle(self, yaw):
+        return yaw * (2 * math.pi / self.calc_yaw_index(math.pi * 2))
 
     @staticmethod
     def calc_heuristic(n1, n2):
-        w = 1.0  # weight of heuristic
-        d = w * math.hypot(n1.x - n2.x, n1.y - n2.y)
-        return d
+        # taking weight of heuristic as 1
+        
+        return  1 * math.hypot(n1.x - n2.x, n1.y - n2.y)
 
     def calc_grid_position(self, index, min_position):
         """
@@ -178,8 +189,19 @@ class AStarPlanner:     # Changes Required
     def calc_xy_index(self, position, min_pos):
         return round((position - min_pos) / self.resolution)
 
+    def calc_yaw_index(self, yaw):
+        """
+        calc yaw index
+
+        :param yaw:
+        :return:
+        """
+        resolution_yaw = math.radians(30)
+        return round(yaw / resolution_yaw)
+
     def calc_grid_index(self, node):
-        return (node.y - self.min_y) * self.x_width + (node.x - self.min_x)
+        return (node.yaw - self.min_y) * self.x_width * self.y_width + \
+               (node.y - self.min_y) * self.x_width + (node.x - self.min_x)
 
     def verify_node(self, node):
         px = self.calc_grid_position(node.x, self.min_x)
@@ -225,15 +247,31 @@ class AStarPlanner:     # Changes Required
 
     @staticmethod
     def get_motion_model():
-        # dx, dy, cost
-        motion = [[1, 0, 1],
+        # dx, dy, dyaw
+        motion = [[1, 0, 0],
+                  [0, 1, 0],
+                  [-1, 0, 0],
+                  [0, -1, 0],
+                  [-1, -1, 0],
+                  [-1, 1, 0],
+                  [1, -1, 0],
+                  [1, 1, 0],
+                  [1, 0, -1],
+                  [0, 1, -1],
+                  [-1, 0, -1],
+                  [0, -1, -1],
+                  [-1, -1, -1],
+                  [-1, 1, -1],
+                  [1, -1, -1],
+                  [1, 1, -1],
+                  [1, 0, 1],
                   [0, 1, 1],
                   [-1, 0, 1],
                   [0, -1, 1],
-                  [-1, -1, math.sqrt(2)],
-                  [-1, 1, math.sqrt(2)],
-                  [1, -1, math.sqrt(2)],
-                  [1, 1, math.sqrt(2)]]
+                  [-1, -1, 1],
+                  [-1, 1, 1],
+                  [1, -1, 1],
+                  [1, 1, 1]]
 
         return motion
 
@@ -257,11 +295,12 @@ class PathPlanning:
         self.robot_radius = 4
         self.a_star = AStarPlanner(self.ox, self.oy, self.grid_size, self.robot_radius)
 
-    def plan_path(self,sx, sy, gx, gy):    
-        rx, ry = self.a_star.planning(sx+self.margin, sy+self.margin, gx+self.margin, gy+self.margin)
-        rx = np.array(rx)-self.margin+0.5
-        ry = np.array(ry)-self.margin+0.5
-        path = np.vstack([rx,ry]).T
+    def plan_path(self, sx, sy, gx, gy):
+        rx, ry, ryaw = self.a_star.planning(sx + self.margin, sy + self.margin, gy, gx + self.margin, gy + self.margin, 0)
+        # Ensure all required arguments are provided, including placeholders for yaw angles
+        rx = np.array(rx) - self.margin + 0.5
+        ry = np.array(ry) - self.margin + 0.5
+        path = np.vstack([rx, ry]).T
         return path[::-1]
 
 ############################################### Park Path Planner #################################################
@@ -285,14 +324,14 @@ class ParkPathPlanning:
         self.robot_radius = 4
         self.a_star = AStarPlanner(self.ox, self.oy, self.grid_size, self.robot_radius)
 
-    def generate_park_scenario(self,sx, sy, gx, gy):
-        rx, ry = self.a_star.planning(sx+self.margin, sy+self.margin, gx+self.margin, gy+self.margin)
-        rx = np.array(rx)-self.margin+0.5
-        ry = np.array(ry)-self.margin+0.5
-        path = np.vstack([rx,ry]).T
+    def generate_park_scenario(self, sx, sy, gx, gy):
+        rx, ry, ryaw = self.a_star.planning(sx+self.margin, sy+self.margin, 0, gx+self.margin, gy+self.margin, 0)
+        # The `0` values are placeholder yaw angles. 
+        rx = np.array(rx) - self.margin + 0.5
+        ry = np.array(ry) - self.margin + 0.5
+        path = np.vstack([rx, ry]).T
         path = path[::-1]
-        computed_angle = angle_of_line(path[-10][0],path[-10][1],path[-1][0],path[-1][1])
-
+        computed_angle = angle_of_line(path[-10][0], path[-10][1], path[-1][0], path[-1][1])
         s = 4
         l = 8
         d = 2
